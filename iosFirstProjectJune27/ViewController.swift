cc954fc4ccb6d0707912758608e2cdc9d34c054c
Gurwinder Singh <gurwinder958652@gmail.com>
2019-06-28 18:49:13 -0400

    [ Gurwinder ] Alert bocagesx created

--- iosFirstProjectJune27/ViewController.swift
+++ iosFirstProjectJune27/ViewController.swift
@@ -11,16 +11,41 @@ import UIKit
 class ViewController: UIViewController
 {
 
+    @IBOutlet weak var labelToShow: UILabel!
     @IBOutlet weak var txtTextField2: UITextField!
     @IBOutlet weak var txtTextField: UITextField!
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        self.title = "Login"
     }
 
-   
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        self.validateUser()
+    }
     
-
+    @IBAction func loginClick(_ sender: UIButton) {
+    }
+    func validateUser(){
+        if (txtTextField.text == "admin@gmail.com"  && txtTextField2.text == "admin123") {
+            self.labelToShow.text = "Successful"
+            
+        }
+        else{
+            // First Step
+            // Create alert controller object
+            let alert = UIAlertController(title: "Error", message: "User Email/ Password Incorrect", preferredStyle:UIAlertController.Style.alert)
+            
+            // Ok button Add
+            let actionOK = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+            
+            // add button to alert
+            alert.addAction(actionOK)
+            // present alert to the user
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
 
 }
 
